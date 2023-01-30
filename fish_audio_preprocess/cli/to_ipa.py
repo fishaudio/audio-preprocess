4d295fba0b0416e59f8768c45a4dbe0aa8223495
LordElf <shawnlai.cn@gmail.com>
2023-01-30 00:16:23 -0500

    reformatted

--- fish_audio_preprocess/cli/to_ipa.py
+++ fish_audio_preprocess/cli/to_ipa.py
@@ -2,6 +2,7 @@ from fish_audio_preprocess.utils.to_ipa import chinese2p
 
 import click
 
+
 @click.command()
 @click.argument("input_file", type=click.Path(exists=True))
 @click.argument("pinyin_labels_file", type=click.Path(exists=False))
@@ -13,7 +14,6 @@ import click
     default=8,
     help="Number of workers to use for processing.",
 )
-
 def to_ipa(input_file, pinyin_labels_file, phoneme_labels_file, num_workers):
     """Convert Chinese characters to pinyin and phonemes."""
     chinese2p(input_file, pinyin_labels_file, phoneme_labels_file, num_workers)
